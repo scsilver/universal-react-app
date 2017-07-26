d6173552765dc5c129a441762f0f2c45121d1a0a
Scott Silverstein <scott@Scotts-MacBook-Pro.local>
2017-07-25 23:15:19 -0600

    added cat and style sheet

--- sharedComponents/HelloWorld.jsx
+++ sharedComponents/HelloWorld.jsx
@@ -1,19 +1,30 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import { View, Text, Image, StyleSheet } from 'react-primitives';
+import { View, Text, Image, StyleSheet } from "react-primitives";
 
 class HelloWorld extends Component {
-    render() {
-        return (
-            <View>
-                <Text>Hello World</Text>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View>
+        <Image
+          style={styles.image}
+          source="https://community.cadence.com/resized-image/__size/940x0/__key/communityserver-blogs-components-weblogfiles/00-00-00-01-06/4544.kitten.jpg"
+        />
+        <Text style={styles.text}>Hello World</Text>
+      </View>
+    );
+  }
 }
-
-HelloWorld.propTypes = {
-};
+const styles = StyleSheet.create({
+  image: {
+    height: 50,
+    width: 50
+  },
+  text: {
+    fontSize: 20
+  }
+});
+HelloWorld.propTypes = {};
 
 export default HelloWorld;
